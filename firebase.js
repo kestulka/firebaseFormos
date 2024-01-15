b06805d8ad5ff19b537c2f0e16ee6a575fa5c087
kestulka <kgirdziunas0@gmail.com>
2024-01-15 16:18:50 +0200

    insert, update

--- firebase.js
+++ firebase.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDXrTkTMEWsRJCalSMyKcb0UDlMyk3ICMI",
-    authDomain: "formprojeect.firebaseapp.com",
-    projectId: "formprojeect",
-    storageBucket: "formprojeect.appspot.com",
-    messagingSenderId: "334861397134",
-    appId: "1:334861397134:web:ec7df8804ad0c9fb706ddf"
-  };
+  apiKey: "AIzaSyDXrTkTMEWsRJCalSMyKcb0UDlMyk3ICMI",
+  authDomain: "formprojeect.firebaseapp.com",
+  databaseURL: "https://formprojeect-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "formprojeect",
+  storageBucket: "formprojeect.appspot.com",
+  messagingSenderId: "334861397134",
+  appId: "1:334861397134:web:ec7df8804ad0c9fb706ddf"
+};
