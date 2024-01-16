e148a7e2a1f611a5517165dbd731717dd5c63954
kestulka <kgirdiunas0@gmail.com>
2024-01-16 04:32:36 +0200

    visual only

--- firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDXrTkTMEWsRJCalSMyKcb0UDlMyk3ICMI",
-  authDomain: "formprojeect.firebaseapp.com",
-  databaseURL: "https://formprojeect-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "formprojeect",
-  storageBucket: "formprojeect.appspot.com",
-  messagingSenderId: "334861397134",
-  appId: "1:334861397134:web:ec7df8804ad0c9fb706ddf"
-};
---
+++ klases/script.js
@@ -0,0 +1,85 @@
+import { firebaseConfig } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  child,
+  get,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+const idDiv = document.getElementById("id");
+const nameDiv = document.getElementById("name");
+const emailDiv = document.getElementById("email");
+const genderDiv = document.getElementById("gender");
+
+const idInput = document.createElement("input");
+idInput.type = "number";
+idInput.placeholder = "Enter your id";
+idDiv.appendChild(idInput);
+
+const nameInput = document.createElement("input");
+nameInput.type = "text";
+nameInput.placeholder = "Enter your name";
+nameDiv.appendChild(nameInput);
+
+const emailInput = document.createElement("input");
+emailInput.type = "email";
+emailInput.placeholder = "Enter your email";
+emailDiv.appendChild(emailInput);
+
+const genderInput = document.createElement("input");
+genderInput.type = "text";
+genderInput.placeholder = "Enter your gender";
+genderDiv.appendChild(genderInput);
+
+const insertBtn = document.getElementById("insert");
+const updateBtn = document.getElementById("update");
+const removeBtn = document.getElementById("remove");
+
+insertBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const userId = idInput.value;
+  const userName = nameInput.value;
+  const userEmail = emailInput.value;
+  const userGender = genderInput.value;
+
+  set(ref(db, "users/" + userId), {
+    id: userId,
+    username: userName,
+    email: userEmail,
+    gender: userGender,
+  })
+    .then(() => {
+      alert("user added sucessfully");
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
+
+updateBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const userId = idInput.value;
+
+  get(child(ref(db), `users/${userId}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        idInput.value = snapshot.val().id;
+        nameInput.value = snapshot.val().name;
+        emailInput.value = snapshot.val().email;
+        genderInput.value = snapshot.val().gender;
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
